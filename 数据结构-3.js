cc55d0d055dcdc2ba8893ddbe151875f6cdf603c
Lideqiang2019 <17805422918@163.com>
2021-01-11 21:36:58 +0800

    数据结构二叉树刷题

--- 数据结构-3.js
+++ 数据结构-3.js
@@ -109,3 +109,112 @@ var lowestCommonAncestor = function(root, p, q) {
     }
     return left==null?right:left
 };
+
+/**
+ * 
+ * @param {*} head 
+ * 回文链表
+ */
+var isPalindrome = function(head) {
+    // 用栈来解决，这里的递归可以产生栈
+    let left = head;
+    let res = true;
+    return traverse(head)
+    function traverse(right){
+        if(right==null) return true
+        // 先压栈，即递归走到链表最右边
+        let res = traverse(right.next)
+        res = res&&(left.val===right.val)
+        left = left.next
+        return res
+    }
+};
+
+
+var isPalindrome = function(head) {
+    // 用栈来解决，这里的递归可以产生栈
+    /*
+    let left = head;
+    let res = true;
+    return traverse(head)
+    function traverse(right){
+        if(right==null) return true
+        // 先压栈，即递归走到链表最右边
+        let res = traverse(right.next)
+        res = res&&(left.val===right.val)
+        left = left.next
+        return res
+    }
+     */
+    /**
+     * 不用额外的栈空间，即在原链表上判断是否为回文链表。
+     * 利用快慢指针，慢指针找到链表的中间位置，然后两边往里面判断是否为回文链表
+     */
+    let slow = head, fast = head;
+    while(fast!=null && fast.next!=null){
+        let p = slow // 用于换原链表
+        slow = slow.next
+        fast = fast.next.next
+    }
+    if(fast!=null) {
+        p = slow
+        slow = slow.next // 如果是奇数，中间的节点不应该反转
+    }
+
+    let left = head
+    let right = traverse(slow) // 将slow后的链表反转
+    while(right!=null){
+        if(left.val!==right.val) return false
+        left = left.next
+        right = right.next
+    }
+    p.next = traverse(q)
+    return true
+
+    function traverse(head){// 反转链表
+        let pre = null, cur = head
+        while(cur!=null){
+            let next = cur.next
+            cur.next = pre
+            pre = cur
+            cur =  next
+        }
+        return pre
+    }
+};
+
+/**
+ * 
+ * @param {*} head 
+ * @param {*} k 
+ * 反转k个链表
+ */
+var reverseKGroup = function(head, k) {
+    /**
+     * 这道题目有子问题的感觉，可以开启递归模式，不过head需要变一下
+     * 理解reverseKGroup应该返回的是反转的头节点
+     */
+    let a = head, b = head
+    let newNode = null
+    for(let i=0;i<k;i++){
+        if(b==null){
+            return head
+        }
+        b =  b.next
+    }  
+    newNode = reverse(a,b) // 前k个节点的反转成功
+    a.next = reverseKGroup(b,k)
+    return newNode
+};
+
+function reverse(a,b){
+    // 反转[a,b)之间的链表
+    let pre=null, cur = a, nxt = a
+    while(cur!=b){
+        nxt = cur.next
+        cur.next = pre
+        pre = cur
+        cur = nxt
+    }
+    return pre
+}
